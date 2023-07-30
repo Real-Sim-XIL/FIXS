703296bf557c87e0647f427ffa91ab0863a7d91e
Yunli Shao <shaoy@ornl.gov>
2023-07-30 17:22:29 -0400

    add tmp handy debug projects for RS development

---
+++ DebugRS/tmpProj/MathLibrary.h
@@ -0,0 +1,23 @@
+// MathLibrary.h
+#pragma once
+
+namespace MathLibraryaaa
+{
+    class Arithmetic
+    {
+    public:
+        Arithmetic();
+
+        // Returns a + b
+        static double Add(double a, double b);
+
+        // Returns a - b
+        static double Subtract(double a, double b);
+
+        // Returns a * b
+        static double Multiply(double a, double b);
+
+        // Returns a / b
+        static double Divide(double a, double b);
+    };
+}
---
+++ DebugRS/tmpProj/mainDebug.c
@@ -0,0 +1,5 @@
+int main(int argc, char* argv[]) {
+	system("D:\\src_ornl\\RealSimDspace\\HIL-RealSim-Operation\\src_cm4sl\\runSumoIpgSimulink.bat");
+
+	return 0;
+}
