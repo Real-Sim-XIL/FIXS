094bac0a6985080b4a010b99b575352dbd32f934
Yunli Shao <yunli.shao@uga.edu>
2023-08-23 15:31:03 -0400

    add application to test signal light with IPG

---
+++ tests/SignalIpg/runShallowfordRd.bat
@@ -0,0 +1,6 @@
+:: "C:\Users\y0n\Dropbox (ORNL)\2_projects\1_2_sumo\1_8_signalcontroller\sumo_nema-main\bin\sumo-guiD.exe" -c ./ShallowfordRd_RL.sumocfg --remote-port 1337 --step-length 0.1 --start
+::sumo-gui -c ./ShallowfordRd_RL.sumocfg --remote-port 1337 --step-length 0.1 --start
+::sumo-gui -c ./ShallowfordRd_RL.sumocfg --begin 39600
+sumo-gui -c ./sumoFiles/ShallowfordRd_RL.sumocfg --remote-port 1337 --step-length 0.1 --start --begin 39600
+::sumo-gui -c ./ShallowfordRd_RL_test.sumocfg --start
+::sumo-gui -c ./ShallowfordRd_RL_test.sumocfg --remote-port 1337 --step-length 0.1 --start
