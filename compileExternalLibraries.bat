02423c2213a52101e70b6273bf286b368ecb5867
Yunli Shao <realsimxil@gmail.com>
2023-09-06 00:08:25 -0400

    compileExternalLibraries skip create build folder if it exist

--- compileExternalLibraries.bat
+++ compileExternalLibraries.bat
@@ -1,12 +1,15 @@
 cd .\CommonLib\libevent
-md build && cd build
+if not exist build md build
+cd build
 cmake -G "Visual Studio 16 2019" -DEVENT__DISABLE_MBEDTLS=ON ..  
 cmake --build . --config Release 
 cmake --build . --config Debug
 
-pause
-REM cd .\CommonLib\yaml-cpp
-REM md build && cd build
-REM cmake -G "Visual Studio 16 2019" ..  
-REM cmake --build . --config Release
-REM cmake --build . --config Debug
+cd ..\..\..\
+cd .\CommonLib\yaml-cpp
+if not exist build md build
+cd build
+cmake -G "Visual Studio 16 2019" ..  
+cmake --build . --config Release
+cmake --build . --config Debug
+
