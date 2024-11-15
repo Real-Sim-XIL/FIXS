5d9df43f72cd4077fdc391dfd234228be3d522c0
Yunli Shao <yunli.shao@uga.edu>
2024-11-15 10:48:19 -0500

    remove proprietary files

--- tests/runTests.bat
+++
@@ -1,8 +0,0 @@
-if exist testsResults.log del testsResults.log
-echo =============================================>> testsResults.log
-echo ===^> Run Tests At %DATE% %TIME% >> testsResults.log
-echo =============================================>> testsResults.log
-
-cd .\CoordMerge
-call runCoordMergeSUMO.bat
-
--- tests/runTests.py
+++
@@ -1,5 +0,0 @@
-import os
-import subprocess
-
-subprocess.Popen('.\CoordMerge\runCoordMerge.bat')
-os.system('.\CoordMerge\runCoordMerge.bat')
