3642c375d5caed961d850c11407fef8bb393b533
Hetian Guo <hetianguoml@gmail.com>
2024-11-24 17:15:17 -0500

    Update ConfigHelper.py

--- CommonLib/ConfigHelper.py
+++ CommonLib/ConfigHelper.py
@@ -24,11 +24,11 @@ s_mapTypeValues = {
 class ConfigHelper:
     def __init__(self):
         # Initialize the s_mapTypeValues
-        self.SimulationSetup = defaultdict(lambda: None)
-        self.ApplicationSetup = defaultdict(lambda: None)
-        self.XilSetup = defaultdict(lambda: None)
-        self.CarMakerSetup = defaultdict(lambda: None)
-        self.SumoSetup = defaultdict(lambda: None)
+        self.simulation_setup = defaultdict(lambda: None)
+        self.application_setup = defaultdict(lambda: None)
+        self.Xil_setup = defaultdict(lambda: None)
+        self.CarMaker_setup = defaultdict(lambda: None)
+        self.Sumo_setup = defaultdict(lambda: None)
         
     def getConfig(self, configName):
         path = os.path.normpath(configName)
@@ -36,38 +36,38 @@ class ConfigHelper:
             config = yaml.safe_load(file)
 
         # Simulation Setup
-        simulation_node = config.get("SimulationSetup", {})
-        self.SimulationSetup["EnableRealSim"] = self.parserFlag(simulation_node, "EnableRealSim", True)
-        self.SimulationSetup["EnableVerboseLog"] = self.parserFlag(simulation_node, "EnableVerboseLog", False)
-        self.SimulationSetup["SimulationEndTime"] = self.parserDouble(simulation_node, "SimulationEndTime", 90000)
-        self.SimulationSetup["EnableExternalDynamics"] = self.parserFlag(simulation_node, "EnableExternalDynamics", False)
-        self.SimulationSetup["SelectedTrafficSimulator"] = self.parserString(simulation_node, "SelectedTrafficSimulator", "SUMO")
-        self.SimulationSetup["TrafficSimulatorIP"] = self.parserString(simulation_node, "TrafficSimulatorIP", "127.0.0.1")
-        self.SimulationSetup["TrafficSimulatorPort"] = self.parserInteger(simulation_node, "TrafficSimulatorPort", 1337)
-        self.SimulationSetup["SimulationMode"] = self.parserInteger(simulation_node, "SimulationMode", 0)
-        self.SimulationSetup["SimulationModeParameter"] = self.parserDouble(simulation_node, "SimulationModeParameter", 0)
-        self.SimulationSetup["VehicleMessageField"] = self.parserStringVector(simulation_node, "VehicleMessageField", ["id", "type", "speed", "positionX", "positionY"])
+        simulation_node = config.get("simulation_setup", {})
+        self.simulation_setup["EnableRealSim"] = self.parserFlag(simulation_node, "EnableRealSim", True)
+        self.simulation_setup["EnableVerboseLog"] = self.parserFlag(simulation_node, "EnableVerboseLog", False)
+        self.simulation_setup["SimulationEndTime"] = self.parserDouble(simulation_node, "SimulationEndTime", 90000)
+        self.simulation_setup["EnableExternalDynamics"] = self.parserFlag(simulation_node, "EnableExternalDynamics", False)
+        self.simulation_setup["SelectedTrafficSimulator"] = self.parserString(simulation_node, "SelectedTrafficSimulator", "SUMO")
+        self.simulation_setup["TrafficSimulatorIP"] = self.parserString(simulation_node, "TrafficSimulatorIP", "127.0.0.1")
+        self.simulation_setup["TrafficSimulatorPort"] = self.parserInteger(simulation_node, "TrafficSimulatorPort", 1337)
+        self.simulation_setup["SimulationMode"] = self.parserInteger(simulation_node, "SimulationMode", 0)
+        self.simulation_setup["SimulationModeParameter"] = self.parserDouble(simulation_node, "SimulationModeParameter", 0)
+        self.simulation_setup["VehicleMessageField"] = self.parserStringVector(simulation_node, "VehicleMessageField", ["id", "type", "speed", "positionX", "positionY"])
         # Sumo Setup
-        sumo_node = config.get("SumoSetup", {})
-        self.SumoSetup["SpeedMode"] = sumo_node.get("SpeedMode", 0)
+        sumo_node = config.get("Sumo_setup", {})
+        self.Sumo_setup["SpeedMode"] = sumo_node.get("SpeedMode", 0)
         # Application Setup
-        app_node = config.get("ApplicationSetup", {})
-        self.ApplicationSetup["EnableApplicationLayer"] = self.parserFlag(app_node, "EnableApplicationLayer", False)
-        self.ApplicationSetup["VehicleSubscription"] = self.parseVehicleSubscription(app_node, "VehicleSubscription", [])
+        app_node = config.get("application_setup", {})
+        self.application_setup["EnableApplicationLayer"] = self.parserFlag(app_node, "EnableApplicationLayer", False)
+        self.application_setup["VehicleSubscription"] = self.parseVehicleSubscription(app_node, "VehicleSubscription", [])
 
         # Xil Setup
-        xil_node = config.get("XilSetup", {})
-        self.XilSetup["EnableXil"] = xil_node.get("EnableXil", False)
-        self.XilSetup["VehicleSubscription"] = self.parseVehicleSubscription(xil_node, "VehicleSubscription", [])
+        xil_node = config.get("Xil_setup", {})
+        self.Xil_setup["EnableXil"] = xil_node.get("EnableXil", False)
+        self.Xil_setup["VehicleSubscription"] = self.parseVehicleSubscription(xil_node, "VehicleSubscription", [])
 
         return 0
 
     def resetConfig(self):
         # Clear all config settings
-        self.SimulationSetup.clear()
-        self.ApplicationSetup.clear()
-        self.XilSetup.clear()
-        self.CarMakerSetup.clear()
+        self.simulation_setup.clear()
+        self.application_setup.clear()
+        self.Xil_setup.clear()
+        self.CarMaker_setup.clear()
 
     def parserFlag(self, node, name, default=False):
         return node.get(name, default) in ['true', True]
@@ -103,7 +103,5 @@ class ConfigHelper:
 
 
 
-
-# Example Usage
-config_helper = ConfigHelper()
-config_helper.getConfig(r"C:\Users\hg25079\Documents\GitHub\FIXS\tests\Applications\Ecodriving\ecodrivingConfig.yaml")
+if __name__ == "__main__":
+    config_helper = ConfigHelper()
